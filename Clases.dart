8a7a60b73f221bfb0558a077d5d3c845088d71f7
khr1stopher <kkromans009@gmail.com>
2020-10-24 16:31:29 -0400

    Constructores con nombre

--- Clases.dart
+++ Clases.dart
@@ -1,10 +1,27 @@
+import 'dart:convert';
+
 void main(List<String> args) {
   // final es igual a una constante y tambien estan los const
-  final IronMan = new Heroe(nombre: "Start", poder: "inteligencia");
+  // final IronMan = new Heroe(nombre: "Start", poder: "inteligencia");
 
-  print(IronMan);
-  print(IronMan.nombre);
-  print(IronMan.poder);
+  // final khristopher = new Heroe(nombre: "khristopher", poder: "Inteligencia");
+
+  final rowJson = '{"nombre": "khristopher", "poder": "inteligencia"}';
+
+  final rowJsonParse = json.decode(rowJson);
+
+  // print(IronMan);
+  // print(IronMan.nombre);
+  // print(IronMan.poder);
+
+  Heroe khristopher = Heroe.fromJson(rowJsonParse);
+
+  // print(khristopher.nombre);
+  // print(khristopher.poder);
+
+  // print(rowJsonParse);
+
+  print(khristopher);
 }
 
 class Heroe {
@@ -18,6 +35,11 @@ class Heroe {
   // forma corta
 
   Heroe({this.nombre, this.poder});
+
+  Heroe.fromJson(Map ParsedJson) {
+    nombre = ParsedJson['nombre'];
+    poder = ParsedJson['poder'];
+  }
 
   String toString() => "nombre: $nombre, poder: $poder";
 }
